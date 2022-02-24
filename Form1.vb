2b4714b8310546c710c9a61139227e9429891d9e
hoowwshii <carvinxwinarli@outlook.com>
2022-02-24 13:25:30 +0700

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Select Case TextBox1.Text
+            Case "1"
+                TextBox2.Text = "Senin"
+            Case "2"
+                TextBox2.Text = "Selasa"
+            Case "3"
+                TextBox2.Text = "Rabu"
+            Case "4"
+                TextBox2.Text = "Kamis"
+            Case "5"
+                TextBox2.Text = "Jumat"
+            Case "6"
+                TextBox2.Text = "Sabtu"
+            Case "7"
+                TextBox2.Text = "Minggu"
+            Case Else
+                TextBox2.Text = "Isi hari dengan benar, antara 1 - 7"
+        End Select
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Close()
+    End Sub
+End Class
---
+++ Form2.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form2
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim nilai As Integer
+        nilai = TextBox1.Text
+        Dim ket As String
+
+        Select Case nilai
+            Case 90 To 100
+                ket = "Lulus, A"
+            Case 80 To 89
+                ket = "Lulus, B"
+            Case 70 To 79
+                ket = "Lulus, C"
+            Case 60 To 69
+                ket = "Tidak Lulus, D"
+            Case 0 To 59
+                ket = "Tidak Lulus, F"
+        End Select
+        TextBox2.Text = ket
+    End Sub
+End Class
